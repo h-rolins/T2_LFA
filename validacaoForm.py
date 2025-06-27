16efefa4b3d186a0cc8a8fa8f8c5451cfd77fb06
neci <necigalli@gmail.com>
2025-06-26 22:31:03 -0300

    email corrigido

--- validacaoForm.py
+++ validacaoForm.py
@@ -7,7 +7,7 @@ def validar_cpf(cpf):
     return bool(re.fullmatch(r'\d{11}|\d{3}\.\d{3}\.\d{3}-\d{2}', cpf))
 
 def validar_email(email):
-    return bool(re.fullmatch(r'[a-zA-Z0-9._]{2,}@[a-zA-Z0-9._]{2,}\.[a-z]{3}', email))
+    return bool(re.fullmatch(r'[a-zA-Z0-9._]{2,}@[a-zA-Z]{1,50}\.[a-z]{3}', email))
 
 def validar_telefone(telefone):
     return bool(re.fullmatch(r'\d{11}|\(\d{2}\)\d{5}-\d{4}', telefone))
@@ -27,7 +27,7 @@ with open("texto.txt", "r", encoding="utf-8") as arquivo:
     texto = arquivo.read()
 
 
-padrao_email = r'[\w\._]{2,}@[\w\._]+\.[a-z]{3}'
+padrao_email = r'[a-zA-Z0-9._]{2,}@[a-zA-Z]{1,50}\.[a-z]{3}'
 emails_encontrados = re.findall(padrao_email, texto)
 
 print("\nE-mails válidos extraídos do texto:")
